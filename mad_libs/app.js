a61082dff0f1e83b08b2fffdaacd80fe7965f4c2
zitzmah <haleyzitzmann@gmail.com>
2023-11-21 08:15:29 -0700

    start mad lib function created

--- mad_libs/app.js
+++ mad_libs/app.js
@@ -5,3 +5,19 @@ let words = {
     adverb: "",
     anotherAdjective: ""
 }
+
+const startMadLib = () => {
+    const num = prompt("Give me a number");
+    words.number = num
+    const adj = prompt("Give me an adjective");
+    words.adjective = adj
+    const plurNoun = prompt("Give me a plural noun")
+    words.pluralNoun = plurNoun
+    const adv = prompt("Give me an adverb")
+    words.adverb = adv
+    const anotherAdj = prompt("Give me another adjective")
+    words.anotherAdjective = anotherAdj
+}
+
+startMadLib()
+console.log(words)
