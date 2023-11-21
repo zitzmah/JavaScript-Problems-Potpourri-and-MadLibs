272678e1bc61734e12be04da5c54f10b98713aef
zitzmah <haleyzitzmann@gmail.com>
2023-11-21 07:45:29 -0700

    bank account summary completed

--- solutions.js
+++ solutions.js
@@ -29,3 +29,32 @@ const oddNumberCount = (number) => {
 const trollsBeGone = (trollTalk) => {
     return (trollTalk.replace(/[aeiou]/g, ""))
 }
+
+/************************************* */
+//  BANK ACCOUNT SUMMARY
+/************************************* */
+
+const bankInfo = {
+    savings: 500,
+    checking: 500,
+    moneyMarket: 500,
+    creditCard: 500
+}
+
+const bankAccountSummary = (bankAccounts) => {
+    let sum = 0
+    const accounts = Object.values(bankAccounts)
+    accounts.forEach(account => { sum += account })
+    return sum
+}
+
+let bankTotal = bankAccountSummary(bankInfo)
+
+const inTheRed = (total) => {
+    if (total < 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
